7dae2218b41b0cf067288216a443b854f9b7533b
Bill Parrott <bill@chimericdream.com>
2022-01-07 23:14:18 -0600

    feat(Compressed Cobble): added compressed cobblestone up to 4x

--- src/main/java/com/worldofpannotia/minecraft/PannotiasParcelsMod.java
+++ src/main/java/com/worldofpannotia/minecraft/PannotiasParcelsMod.java
@@ -2,10 +2,13 @@ package com.worldofpannotia.minecraft;
 
 import com.worldofpannotia.minecraft.blocks.CaneBundleBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
@@ -21,6 +24,11 @@ public class PannotiasParcelsMod implements ModInitializer {
 
 	public static final Block CANE_BUNDLE_BLOCK = new CaneBundleBlock();
 
+	public static final Block COMPRESSED_COBBLE_1X = new Block(FabricBlockSettings.of(Material.STONE).strength(6f, 18f).sounds(BlockSoundGroup.STONE));
+	public static final Block COMPRESSED_COBBLE_2X = new Block(FabricBlockSettings.of(Material.STONE).strength(18f, 54f).sounds(BlockSoundGroup.STONE));
+	public static final Block COMPRESSED_COBBLE_3X = new Block(FabricBlockSettings.of(Material.STONE).strength(54f, 162f).sounds(BlockSoundGroup.STONE));
+	public static final Block COMPRESSED_COBBLE_4X = new Block(FabricBlockSettings.of(Material.STONE).strength(162f, 486f).sounds(BlockSoundGroup.STONE));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -30,6 +38,15 @@ public class PannotiasParcelsMod implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
 
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "cane_bundle"), CANE_BUNDLE_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_1x"), COMPRESSED_COBBLE_1X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_2x"), COMPRESSED_COBBLE_2X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_3x"), COMPRESSED_COBBLE_3X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_4x"), COMPRESSED_COBBLE_4X);
+
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "cane_bundle"), new BlockItem(CANE_BUNDLE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_1x"), new BlockItem(COMPRESSED_COBBLE_1X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_2x"), new BlockItem(COMPRESSED_COBBLE_2X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_3x"), new BlockItem(COMPRESSED_COBBLE_3X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_4x"), new BlockItem(COMPRESSED_COBBLE_4X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
