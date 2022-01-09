6f936b2349eb6a6e2d97b6d2e9d09546bb730174
Bill Parrott <bill@chimericdream.com>
2022-01-08 23:48:21 -0600

    feat: add a bunch more types of compressed block

--- src/main/java/com/worldofpannotia/minecraft/PannotiasParcelsMod.java
+++ src/main/java/com/worldofpannotia/minecraft/PannotiasParcelsMod.java
@@ -1,7 +1,6 @@
 package com.worldofpannotia.minecraft;
 
-import com.worldofpannotia.minecraft.blocks.CaneBundleBlock;
-import com.worldofpannotia.minecraft.blocks.CompressedCobbleBlock;
+import com.worldofpannotia.minecraft.blocks.*;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -20,31 +19,74 @@ public class PannotiasParcelsMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
+	public static final Block BAMBOO_BUNDLE_BLOCK = new BambooBundleBlock();
 	public static final Block CANE_BUNDLE_BLOCK = new CaneBundleBlock();
+	public static final Block STICK_BUNDLE_BLOCK = new StickBundleBlock();
 
 	public static final Block COMPRESSED_COBBLE_1X = new CompressedCobbleBlock(1);
 	public static final Block COMPRESSED_COBBLE_2X = new CompressedCobbleBlock(2);
 	public static final Block COMPRESSED_COBBLE_3X = new CompressedCobbleBlock(3);
 	public static final Block COMPRESSED_COBBLE_4X = new CompressedCobbleBlock(4);
 
+	public static final Block COMPRESSED_DIRT_1X = new CompressedDirtBlock(1);
+	public static final Block COMPRESSED_DIRT_2X = new CompressedDirtBlock(2);
+	public static final Block COMPRESSED_DIRT_3X = new CompressedDirtBlock(3);
+	public static final Block COMPRESSED_DIRT_4X = new CompressedDirtBlock(4);
+
+	public static final Block COMPRESSED_GRAVEL_1X = new CompressedGravelBlock(1);
+	public static final Block COMPRESSED_GRAVEL_2X = new CompressedGravelBlock(2);
+	public static final Block COMPRESSED_GRAVEL_3X = new CompressedGravelBlock(3);
+	public static final Block COMPRESSED_GRAVEL_4X = new CompressedGravelBlock(4);
+
+	public static final Block COMPRESSED_SAND_1X = new CompressedSandBlock(1);
+	public static final Block COMPRESSED_SAND_2X = new CompressedSandBlock(2);
+	public static final Block COMPRESSED_SAND_3X = new CompressedSandBlock(3);
+	public static final Block COMPRESSED_SAND_4X = new CompressedSandBlock(4);
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Registering blocks for Pannotia's Parcels");
 
-		LOGGER.info("Hello Fabric world!");
-
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "bamboo_bundle"), BAMBOO_BUNDLE_BLOCK);
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "cane_bundle"), CANE_BUNDLE_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "stick_bundle"), STICK_BUNDLE_BLOCK);
+
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_1x"), COMPRESSED_COBBLE_1X);
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_2x"), COMPRESSED_COBBLE_2X);
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_3x"), COMPRESSED_COBBLE_3X);
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_cobblestone_4x"), COMPRESSED_COBBLE_4X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_dirt_1x"), COMPRESSED_DIRT_1X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_dirt_2x"), COMPRESSED_DIRT_2X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_dirt_3x"), COMPRESSED_DIRT_3X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_dirt_4x"), COMPRESSED_DIRT_4X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_gravel_1x"), COMPRESSED_GRAVEL_1X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_gravel_2x"), COMPRESSED_GRAVEL_2X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_gravel_3x"), COMPRESSED_GRAVEL_3X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_gravel_4x"), COMPRESSED_GRAVEL_4X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_sand_1x"), COMPRESSED_SAND_1X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_sand_2x"), COMPRESSED_SAND_2X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_sand_3x"), COMPRESSED_SAND_3X);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "compressed_sand_4x"), COMPRESSED_SAND_4X);
 
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "bamboo_bundle"), new BlockItem(BAMBOO_BUNDLE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "cane_bundle"), new BlockItem(CANE_BUNDLE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "stick_bundle"), new BlockItem(STICK_BUNDLE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_1x"), new BlockItem(COMPRESSED_COBBLE_1X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_2x"), new BlockItem(COMPRESSED_COBBLE_2X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_3x"), new BlockItem(COMPRESSED_COBBLE_3X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_cobblestone_4x"), new BlockItem(COMPRESSED_COBBLE_4X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_dirt_1x"), new BlockItem(COMPRESSED_DIRT_1X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_dirt_2x"), new BlockItem(COMPRESSED_DIRT_2X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_dirt_3x"), new BlockItem(COMPRESSED_DIRT_3X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_dirt_4x"), new BlockItem(COMPRESSED_DIRT_4X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_gravel_1x"), new BlockItem(COMPRESSED_GRAVEL_1X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_gravel_2x"), new BlockItem(COMPRESSED_GRAVEL_2X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_gravel_3x"), new BlockItem(COMPRESSED_GRAVEL_3X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_gravel_4x"), new BlockItem(COMPRESSED_GRAVEL_4X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_sand_1x"), new BlockItem(COMPRESSED_SAND_1X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_sand_2x"), new BlockItem(COMPRESSED_SAND_2X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_sand_3x"), new BlockItem(COMPRESSED_SAND_3X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "compressed_sand_4x"), new BlockItem(COMPRESSED_SAND_4X, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
